220bed72e86e0bffa2190252f069575f96b2c877
Константин Голенков <golenkovkostya@gmail.com>
2014-10-31 01:44:10 +0500

    second worked version with images

--- src/com/example/galleryTwo/ui/MainActivity.java
+++ src/com/example/galleryTwo/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.galleryTwo.ui;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,12 @@ import android.support.v4.view.ViewPager;
 import com.example.galleryTwo.Const;
 import com.example.galleryTwo.bean.MyImageBean;
 import com.example.galleryTwo.R;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 public class MainActivity extends FragmentActivity {
 
@@ -22,6 +29,16 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		File cacheDir = StorageUtils.getCacheDirectory(getApplicationContext());
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+		.diskCache(new UnlimitedDiscCache(cacheDir))
+		.diskCacheFileNameGenerator(new Md5FileNameGenerator())
+		.diskCacheSize(50 * 1024 * 1024) // 50 Mb
+		.tasksProcessingOrder(QueueProcessingType.LIFO)
+		.build();
+		ImageLoader.getInstance().init(config);
+		
+		
 //		Intent intent = getIntent();
 //		if( intent == null || !intent.hasExtra(Const.IMAGE_LIST_NAME)) {
 //			finish();
@@ -31,16 +48,16 @@ public class MainActivity extends FragmentActivity {
 //		List<MyImageBean> imageList = intent.getParcelableArrayListExtra(Const.IMAGE_LIST_NAME);
 		
 		List<MyImageBean> imageList = new ArrayList<MyImageBean>();
-		imageList.add(new MyImageBean(null, "image 1"));
-		imageList.add(new MyImageBean(null, "image 2"));
-		imageList.add(new MyImageBean(null, "image 3"));
-		imageList.add(new MyImageBean(null, "image 4"));
-		imageList.add(new MyImageBean(null, "image 5"));
-		imageList.add(new MyImageBean(null, "image 6"));
-		imageList.add(new MyImageBean(null, "image 7"));
-		imageList.add(new MyImageBean(null, "image 8"));
-		imageList.add(new MyImageBean(null, "image 9"));
-		imageList.add(new MyImageBean(null, "image 10"));
+		imageList.add(new MyImageBean("https://lh4.googleusercontent.com/-WIuWgVcU3Qw/URqubRVcj4I/AAAAAAAAAbs/YvbwgGjwdIQ/s1024/Antelope%252520Walls.jpg", "image 1"));
+		imageList.add(new MyImageBean("https://lh3.googleusercontent.com/-PyggXXZRykM/URquh-kVvoI/AAAAAAAAAbs/hFtDwhtrHHQ/s1024/Colorado%252520River%252520Sunset.jpg", "image 2"));
+		imageList.add(new MyImageBean("https://lh4.googleusercontent.com/-EIBGfnuLtII/URquqVHwaRI/AAAAAAAAAbs/FA4McV2u8VE/s1024/Grand%252520Teton.jpg", "image 3"));
+		imageList.add(new MyImageBean("http://tabletpcssource.com/wp-content/uploads/2011/05/android-logo.png", "image 4"));
+		imageList.add(new MyImageBean("http://weloveicons.s3.amazonaws.com/icons/100929_applications.png", "image 5"));
+		imageList.add(new MyImageBean("http://androidblaze.com/wp-content/uploads/2011/12/tablet-pc-256x256.jpg", "image 6"));
+		imageList.add(new MyImageBean("http://thecustomizewindows.com/wp-content/uploads/2011/11/Nicest-Android-Live-Wallpapers.png", "image 7"));
+		imageList.add(new MyImageBean("https://lh5.googleusercontent.com/-B1HW-z4zwao/URqvFWYRwUI/AAAAAAAAAbs/8Peli53Bs8I/s1024/The%252520Cave%252520BW.jpg", "image 8"));
+		imageList.add(new MyImageBean("https://lh6.googleusercontent.com/-h-ALJt7kSus/URqvIThqYfI/AAAAAAAAAbs/ejiv35olWS8/s1024/Tokyo%252520Heights.jpg", "image 9"));
+		imageList.add(new MyImageBean("https://lh5.googleusercontent.com/-kI_QdYx7VlU/URqvLXCB6gI/AAAAAAAAAbs/N31vlZ6u89o/s1024/Yet%252520Another%252520Rockaway%252520Sunset.jpg", "image 10"));
 		
 		galleryPageAdapter = new GalleryPageAdapter(getSupportFragmentManager(), imageList);
 		
--- src/com/example/galleryTwo/ui/PageFragment.java
+++ src/com/example/galleryTwo/ui/PageFragment.java
@@ -2,13 +2,21 @@ package com.example.galleryTwo.ui;
 
 import com.example.galleryTwo.R;
 import com.example.galleryTwo.bean.MyImageBean;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class PageFragment extends Fragment {
@@ -24,22 +32,31 @@ public class PageFragment extends Fragment {
 	}
 
 	private MyImageBean imageBean;
+	DisplayImageOptions options;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.imageBean = getArguments().getParcelable(ARG_IMAGE_BEAN);
-		// here start load image
-
+		options = new DisplayImageOptions.Builder()
+				.resetViewBeforeLoading(true)
+				.cacheOnDisk(true)
+				.imageScaleType(ImageScaleType.EXACTLY)
+				.build();
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater,
-			ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 
 		View view = inflater.inflate(R.layout.fragment, container, false);
 		TextView titileViewPage = (TextView) view.findViewById(R.id.titleViewPage);
 		titileViewPage.setText(imageBean.getTitle());
+		
+		ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+		
+		ImageLoader.getInstance().displayImage(imageBean.getUrl(), imageView, options);
+		
 		return view;
 
 	}
