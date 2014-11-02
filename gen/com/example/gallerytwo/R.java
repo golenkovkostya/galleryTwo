67bd973b369eb7c4f69b7730d0f3dab5a8ad6b31
Константин Голенков <golenkovkostya@gmail.com>
2014-11-02 23:06:11 +0500

    implemented progress bar

--- gen/com/example/gallerytwo/R.java
+++ gen/com/example/gallerytwo/R.java
@@ -11,10 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int custom_progress_drawable=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int imageView=0x7f060002;
+        public static final int imageView=0x7f060003;
+        public static final int loading_indicator=0x7f060002;
         public static final int pager=0x7f060000;
         public static final int titleViewPage=0x7f060001;
     }
--- src/com/example/galleryTwo/ui/PageFragment.java
+++ src/com/example/galleryTwo/ui/PageFragment.java
@@ -1,23 +1,23 @@
 package com.example.galleryTwo.ui;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.example.galleryTwo.R;
 import com.example.galleryTwo.bean.MyImageBean;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
-
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 public class PageFragment extends Fragment {
 
@@ -39,10 +39,8 @@ public class PageFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		this.imageBean = getArguments().getParcelable(ARG_IMAGE_BEAN);
 		options = new DisplayImageOptions.Builder()
-				.resetViewBeforeLoading(true)
-				.cacheOnDisk(true)
-				.imageScaleType(ImageScaleType.EXACTLY)
-				.build();
+				.resetViewBeforeLoading(true).cacheOnDisk(true)
+				.imageScaleType(ImageScaleType.EXACTLY).build();
 	}
 
 	@Override
@@ -50,13 +48,43 @@ public class PageFragment extends Fragment {
 			Bundle savedInstanceState) {
 
 		View view = inflater.inflate(R.layout.fragment, container, false);
-		TextView titileViewPage = (TextView) view.findViewById(R.id.titleViewPage);
+		TextView titileViewPage = (TextView) view
+				.findViewById(R.id.titleViewPage);
 		titileViewPage.setText(imageBean.getTitle());
-		
+
+		final ProgressBar pb = (ProgressBar) view
+				.findViewById(R.id.loading_indicator);
+
 		ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
-		
-		ImageLoader.getInstance().displayImage(imageBean.getUrl(), imageView, options);
-		
+
+		ImageLoader.getInstance().displayImage(imageBean.getUrl(), imageView,
+				options, new SimpleImageLoadingListener() {
+
+					@Override
+					public void onLoadingStarted(String arg0, View arg1) {
+						pb.setProgress(0);
+						pb.setVisibility(View.VISIBLE);
+					}
+
+					@Override
+					public void onLoadingFailed(String arg0, View arg1,
+							FailReason arg2) {
+						pb.setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onLoadingComplete(String arg0, View arg1,
+							Bitmap arg2) {
+						pb.setVisibility(View.GONE);
+					}
+				}, new ImageLoadingProgressListener() {
+					@Override
+					public void onProgressUpdate(String imageUri, View view,
+							int current, int total) {
+						pb.setProgress(Math.round(100.0f * current / total));
+					}
+				});
+
 		return view;
 
 	}
